0a9b94f5efdbc5d9176385d958f69c30e9a70722
akwiatkowski <and1rew10@gmail.com>
2020-01-25 15:02:16 +0100

    Init commit.

---
+++ src/test/java/pl/unityt/codereview/CodeReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.unityt.codereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
